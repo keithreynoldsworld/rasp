0d83b531b532afc572a51f4c77b7d6e1009d9a3d
Keith Reynolds <keithreynoldsworld@gmail.com>
2024-09-09 15:56:41 -0500

    Add files via upload

--- tinybark.py
+++ tinybark.py
@@ -10,5 +10,5 @@ import scipy
 synthesiser = pipeline("text-to-speech", "suno/bark")
 
 speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
-
+https://drive.google.com/file/d/1awinZUSQRCvixiRTgd9PqCw2nSi8eqL7/view?usp=sharing
 scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
